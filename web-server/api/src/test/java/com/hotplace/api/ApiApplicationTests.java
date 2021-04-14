0c07a27c18c5b1ce67a661cc8973a3e296cd9a99
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-04-14 15:50:36 +0900

    feat: [back] init project (#10)

---
+++ web-server/api/src/test/java/com/hotplace/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotplace.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
