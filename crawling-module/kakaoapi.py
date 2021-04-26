a55baf09ffb93cbe92cbf905638b5ca2cfd3fe80
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-04-26 22:25:55 +0900

    fix: [크롤링] kakao api 모듈화 (#18)

--- crawling-module/kakaoapi.py
+++ crawling-module/kakaoapi.py
@@ -1,56 +1,66 @@
 import requests
 import config
-import json
 
-
-#url = 'https://dapi.kakao.com/v2/local/search/keyword.json?page=2&size=15&query={}'.format(searching)
 KEYWORD_URL = 'https://dapi.kakao.com/v2/local/search/keyword.json'
 ADDRESS_URL = 'https://dapi.kakao.com/v2/local/search/address'
-
 headers = {"Authorization": "KakaoAK "+config.key}
 
-# 동의 중심을 기준으로 할 때 필요한 request
+"""
+#동의 중심을 기준으로 할 때 필요한 request
 add_params = {'query': '마포구 상수동'}
 add_res = requests.get(ADDRESS_URL, params=add_params, headers=headers).json()
 add_doc = add_res['documents']
 address_info = add_doc[0]['address']
 dong_x, dong_y = address_info['x'], address_info['y']
-
-print(dong_x, dong_y)
-
-# 00동 기준 반경 1키로 검색
-searching = '마포구 카페'
-key_params = {'page': 5, 'size': 15, 'query': searching, 'category_group_code': 'CE7'}
 #'x': dong_x, 'y': dong_y, 'radius': 300
 key_res = requests.get(KEYWORD_URL, params=key_params, headers=headers).json()
+"""
 
-place_list, meta = key_res['documents'], key_res['meta']
-total_count = meta['total_count']
-page_cnt = total_count//15
-if total_count % 15 > 0: page_cnt += 1
 
-# 검색 정보
-print(meta)
+class kakao_api:
+    def __init__(self):
+        self.all_place = []
 
-all_place = []
-i = 1
-is_last = False
-while True:
-    if is_last: break
-    key_params['page'] = i
-    i += 1
-    key_res = requests.get(KEYWORD_URL, params=key_params, headers=headers).json()
-    place_list, meta = key_res['documents'], key_res['meta']
-    print(meta['is_end'])
-    if meta['is_end']: is_last = True
+    def get_code(self, word):
+        if "카페" == word: return "CE7"
+        elif "핫플레이스" == word: return "AT4"
+        else: return "FD6"
 
-    for place_dic in place_list:
-        place_name = place_dic['place_name']
-        all_place.append(place_name)
+    def find(self, gu, dong, keyword):
+        searching = gu + dong + keyword
+        key_params = {'size': 15, 'query': searching, 'category_group_code': self.get_code(keyword)}
 
-for i,place in enumerate(all_place):
-    if i % 10 == 0: print("")
-    print(place, end=" ")
+        all_place = self.all_place
+        i = 1
+        is_last = False
+        while True:
+            if is_last: break
+            key_params['page'] = i
+            i += 1
+            key_res = requests.get(KEYWORD_URL, params=key_params, headers=headers).json()
+            place_list, meta = key_res['documents'], key_res['meta']
+            print(meta['is_end'])
+            if meta['is_end']: is_last = True
+
+            for place_dic in place_list:
+                place_name = place_dic['place_name']
+                id = place_dic['id']
+                phone = place_dic['phone']
+                place_url = place_dic['place_url']
+                road_address_name, address_name = place_dic['road_address_name'], place_dic['address_name']
+                x, y = place_dic['x'], place_dic['y']
+
+                # 쿼리 날린 동과 일치하면 넣음
+                if dong in address_name:
+                    all_place.append({'id': id, 'phone': phone, 'place_name': place_name, 'place_url': place_url,
+                                  'address_name': address_name, 'road_address_name': road_address_name, 'x': x, 'y': y})
+
+
+if __name__ == '__main__':
+    pl = kakao_api()
+    pl.find("마포구", "상수동", "카페")
+    for place in pl.all_place:
+        print(place)
 
 """ 
 CE7 : 카페
@@ -69,10 +79,3 @@ road_address_name 서울 마포구 독막로15길 25
 x 126.921917323662
 y 37.5488001921376
 """
-
-
-# for place_dic in place_list:
-#     print("")
-#     for key in place_dic:
-#         print(key, place_dic[key])
-#     print("")
