bc747c3f2f51ceb818eadb21efcc197862179472
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-06-12 18:59:31 +0900

    feat: [back] 모든 구 정보 가져오기 (#39)

---
+++ web-server/api/src/main/java/com/hotplace/api/gu/controller/GuController.java
@@ -0,0 +1,25 @@
+package com.hotplace.api.gu.controller;
+
+import com.hotplace.api.api_form.ApiForm;
+import com.hotplace.api.gu.service.GuService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static com.hotplace.api.api_form.ApiForm.succeed;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api")
+public class GuController {
+    private final GuService guService;
+
+    @GetMapping("/gu")
+    public ApiForm<List<GuDto>> findAll(){
+        return guService.findAll();
+    }
+
+}
