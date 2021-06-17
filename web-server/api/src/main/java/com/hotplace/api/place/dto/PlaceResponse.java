ff67b2924f69221a602acce9cc700844d7c1b717
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-06-17 23:45:31 +0900

    chore: [back] Dto 별점 및 URL 추가

--- web-server/api/src/main/java/com/hotplace/api/place/dto/PlaceResponse.java
+++ web-server/api/src/main/java/com/hotplace/api/place/dto/PlaceResponse.java
@@ -1,5 +1,6 @@
 package com.hotplace.api.place.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.hotplace.api.place.domain.Place;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +15,27 @@ public class PlaceResponse {
     private String longitude_x;
     private String latitude_y;
 
+    @JsonProperty("naver_star")
+    private Float naverStar;
+
+    @JsonProperty("kakao_star")
+    private Float kakaoStar;
+
+    @JsonProperty("instagram_hashtag")
+    private Integer instagramHashtag;
+
+    @JsonProperty("naver_url")
+    private String naverUrl;
+
+    @JsonProperty("kakao_url")
+    private String kakaoUrl;
+
+    @JsonProperty("instagram_url")
+    private String instagramUrl;
+
+    @JsonProperty("homepage_url")
+    private String homepageUrl;
+
     public PlaceResponse(Place place){
         this.name = place.getName();
         this.address = place.getAddress();
@@ -21,5 +43,12 @@ public class PlaceResponse {
         this.dong = place.getDong().getName();
         this.longitude_x = place.getLongitude_x();
         this.latitude_y = place.getLatitude_y();
+        this.naverStar = place.getNaverStar();
+        this.kakaoStar = place.getKakaoStar();
+        this.instagramHashtag = place.getInstagramHashtag();
+        this.naverUrl = place.getNaverUrl();
+        this.kakaoUrl = place.getKakaoUrl();
+        this.instagramUrl = place.getInstagramUrl();
+        this.homepageUrl = place.getHomepageUrl();
     }
 }
