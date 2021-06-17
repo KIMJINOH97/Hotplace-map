0265fb78ede1451d71a6e5e573eddd503219383b
WHO-A-U <59171080+WHO-A-U@users.noreply.github.com>
2021-06-17 18:30:09 +0900

    fix :[back] gu_id 기준 dong 번호 받아오는기능

--- web-server/api/src/main/java/com/hotplace/api/dong/domain/DongRepository.java
+++ web-server/api/src/main/java/com/hotplace/api/dong/domain/DongRepository.java
@@ -2,5 +2,9 @@ package com.hotplace.api.dong.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DongRepository extends JpaRepository<Dong, Integer> {
+
+    List<Dong> findAllByGuId(Integer id);
 }
