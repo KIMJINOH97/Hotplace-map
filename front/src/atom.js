3c8605e9e75c98e4dfc6a5a8851d3fb955a9e262
WHO-A-U <59171080+WHO-A-U@users.noreply.github.com>
2021-08-20 00:01:51 +0900

    chore : [FRONT] recoil,axios 이용

---
+++ front/src/atom.js
@@ -0,0 +1,26 @@
+import { atom } from 'recoil';
+
+export const urlState = atom({
+  key: 'url',
+  default: 'http://localhost:8080',
+});
+
+export const guState = atom({
+  key: 'gu',
+  default: [],
+});
+
+export const dongState = atom({
+  key: 'dong',
+  default: [],
+});
+
+export const subCategoryState = atom({
+  key: 'subCategory',
+  default: [],
+});
+
+export const placeState = atom({
+  key: 'store_list',
+  default: [],
+});
