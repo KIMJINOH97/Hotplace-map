b59158c7998277648d9af2e482cc4f68cc343632
WHO-A-U <59171080+WHO-A-U@users.noreply.github.com>
2021-08-21 02:19:59 +0900

    refactor : [FRONT] filter select 부분 리팩토링

--- front/src/components/FilterForm.js
+++ front/src/components/FilterForm.js
@@ -1,92 +1,113 @@
 import React, { useState, useRef, useEffect } from 'react';
-import styled from 'styled-components';
-import { Form, Input, InputNumber, Buttonct, Select, Button } from 'antd';
-import { useRecoilState, useRecoilValue } from 'recoil';
-import { dongState, guState, subCategoryState, urlState } from '../atom';
 import axios from 'axios';
-const { Option } = Select;
+import { Button } from 'antd';
+import { useRecoilState } from 'recoil';
 
-const layout = {
-  labelCol: { span: 8 },
-  wrapperCol: { span: 16 },
-};
-const validateMessages = {
-  required: '${label} is required!',
-  types: {
-    email: '${label} is not a valid email!',
-    number: '${label} is not a valid number!',
-  },
-  number: {
-    range: '${label} must be between ${min} and ${max}',
-  },
-};
+import {
+  dongState,
+  guState,
+  queryState,
+  subCategoryState,
+  urlState,
+} from '../atom';
+
+import FilterSelect from './filter/FilterSelect';
 
 const FilterForm = () => {
   const [gu, setGu] = useRecoilState(guState);
   const [dong, setDong] = useRecoilState(dongState);
   const [subCategory, setSubCategory] = useRecoilState(subCategoryState);
   const [url] = useRecoilState(urlState);
+  const [query, setQuery] = useRecoilState(queryState);
+
+  const [curGu, setCurGu] = useState();
+  const [curDong, setCurDong] = useState();
+  const [curSubCategory, setCurSubCategory] = useState();
 
   const guSelectChange = async (value, idx) => {
-    const res = await axios.get(url + '/api/dong/' + idx.key);
-    const resData = res.data;
+    console.log(value, idx);
+    const resData = await getDongListByGuId(idx.key);
     setDong(resData.data);
   };
 
+  async function getGuList() {
+    const res = await axios.get(url + '/api/gu');
+    const resData = res.data;
+    setGu(resData.data);
+    setCurGu(resData.data[0].guName);
+  }
+
+  async function getDongListByGuId(guId) {
+    const res = await axios.get(url + '/api/dong/' + guId);
+    const resData = res.data;
+    setDong(resData.data);
+    setCurDong(resData.data[0].dongName);
+    return resData;
+  }
+
+  async function getSubCategory() {
+    const res = await axios.get(url + '/api/sub_category');
+    const resData = res.data;
+    setSubCategory(resData.data);
+    setCurSubCategory(resData.data[0].name);
+  }
+
+  const onChangeGu = (value, key) => {
+    setCurGu(value);
+    guSelectChange(value, key);
+  };
+
+  const onChangeDong = (value) => {
+    setCurDong(value);
+  };
+
+  const onChangeSubCategory = (value) => {
+    setCurSubCategory(value);
+  };
+
   useEffect(() => {
-    async function getGuList() {
-      const res = await axios.get(url + '/api/gu');
-      const resData = res.data;
-      setGu(resData.data);
-    }
-
-    async function getDongListByGuId(guId) {
-      const res = await axios.get(url + '/api/dong/' + guId);
-      const resData = res.data;
-      setDong(resData.data);
-    }
-
-    async function getSubCategory() {
-      const res = await axios.get(url + '/api/sub_category');
-      const resData = res.data;
-      setSubCategory(resData.data);
-    }
-
     getGuList();
     getDongListByGuId(1);
     getSubCategory();
   }, []);
 
   const onClickEvent = () => {
-    console.log('button click!');
     console.log(gu);
     console.log(dong);
     console.log(subCategory);
+
+    console.log({
+      curGu,
+      curDong,
+      curSubCategory,
+    });
+  };
+
+  const isLoad = () => {
+    return gu.length && dong.length && subCategory.length;
   };
 
   return (
     <>
-      <Select style={{ width: 120 }} onChange={guSelectChange}>
-        {gu.map((val) => (
-          <Option key={val.guId} value={val.guName}>
-            {val.guName}
-          </Option>
-        ))}
-      </Select>
-      <Select style={{ width: 120 }}>
-        {dong.map((val) => (
-          <Option key={val.dongId} value={val.dongName}>
-            {val.dongName}
-          </Option>
-        ))}
-      </Select>
-      <Select style={{ width: 120 }}>
-        {subCategory.map((val) => (
-          <Option key={val.id} value={val.name}>
-            {val.name}
-          </Option>
-        ))}
-      </Select>
+      {isLoad() && (
+        <>
+          <FilterSelect
+            currentSelect={curGu}
+            onChangeMethod={onChangeGu}
+            optionList={gu}
+          />
+          <FilterSelect
+            currentSelect={curDong}
+            onChangeMethod={onChangeDong}
+            optionList={dong}
+          />
+          <FilterSelect
+            currentSelect={curSubCategory}
+            onChangeMethod={onChangeSubCategory}
+            optionList={subCategory}
+          />
+        </>
+      )}
       <Button onClick={onClickEvent}>click!</Button>
     </>
   );
