429c0bd6b5d0f8e91b5e4ecb7c6166a6d8232d28
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-08-24 20:23:56 +0900

    chore: [FRONT] .env 환경변수 설정

--- front/src/api/index.js
+++ front/src/api/index.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
 import initCityApi from './initCityApi';
 import initPlaceApi from './initPlaceApi';
-import dotenv from 'dotenv';
-dotenv.config();
 
 export const Axios = axios.create({
-  baseURL: 'http://3.36.26.169:8080',
+  baseURL: process.env.REACT_APP_SPRING_SERVER_API,
 });
 
 export const cityApi = initCityApi(Axios);
