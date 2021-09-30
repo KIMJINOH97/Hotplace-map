b1b00e70e2188e4e92e8eebed2781cb5746ff0f9
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-10-01 02:12:00 +0900

    feat: [FRONT] 카카오, 네이버 로그인 버튼 틀 구현

--- front/src/screens/HotPlaceMapScreen.js
+++ front/src/screens/HotPlaceMapScreen.js
@@ -2,6 +2,12 @@ import React, { useEffect } from 'react';
 import KakaoMap from '../components/map/KakaoMap';
 import SiderBar from '../components/SideBar';
 import { Layout } from 'antd';
+import styled from 'styled-components';
+
+import KAKAO_LOGIN_SHORT from '../assets/KAKAO_LOGIN_SHORT.png';
+import NAVER_LOGIN from '../assets/NAVER_LOGIN.png';
+
+import LoginButton from '../utils/LoginButton';
 // import 'antd/dist/antd.css';
 
 const { Header, Footer, Content } = Layout;
@@ -16,6 +22,8 @@ const HotPlaceMapScreen = () => {
           style={{ padding: 0, backgroundColor: 'white' }}
         ></Header> */}
         <Content>
+          <LoginButton image={KAKAO_LOGIN_SHORT} right={'10px'} />
+          <LoginButton image={NAVER_LOGIN} width={'100px'} right={'110px'} />
           <KakaoMap></KakaoMap>
         </Content>
         {/* <Footer style={{ textAlign: 'center' }}>Hotplace-map</Footer> */}
@@ -25,3 +33,18 @@ const HotPlaceMapScreen = () => {
 };
 
 export default HotPlaceMapScreen;
+
+// const KakaoButton = styled.div`
+//   position: absolute;
+//   top: 1%;
+//   right: 1%;
+//   width: 90px;
+//   height: 45px;
+//   z-index: 401;
+//   border-radius: 12px;
+// `;
+
+// const KakaoImage = styled.img`
+//   width: 100%;
+//   height: 100%;
+// `;
