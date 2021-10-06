13741f18ffa5fb3f9fb03a5096d65ee3c89a7668
WHO-A-U <59171080+WHO-A-U@users.noreply.github.com>
2021-10-07 01:11:29 +0900

    update : CORS uri (react 테스트 환경 추가)

--- web-server/api/src/main/java/com/hotplace/api/config/WebMvcConfig.java
+++ web-server/api/src/main/java/com/hotplace/api/config/WebMvcConfig.java
@@ -10,7 +10,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080/**","https://accounts.kakao.com/**");
+                .allowedOrigins("http://localhost:8080/**",
+                        "https://accounts.kakao.com/**",
+                        "http://localhost:3000/**",
+                        "http://localhost:3000");
     }
 
 }
