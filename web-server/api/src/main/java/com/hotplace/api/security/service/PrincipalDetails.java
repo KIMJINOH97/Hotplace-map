d5518d23b48f4f0be575370f2881c041fa89edc5
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-10-10 16:26:21 +0900

    feat: [back] handler의 getName()을 kakao, naver 두 곳 다 가능하게 하기 위해 Principal 객체 이용 (#92)

---
+++ web-server/api/src/main/java/com/hotplace/api/security/service/PrincipalDetails.java
@@ -0,0 +1,39 @@
+package com.hotplace.api.security.service;
+
+import com.hotplace.api.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Map;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class PrincipalDetails implements OAuth2User {
+
+    private User user;
+    private Map<String, Object> attributes;
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return user.getName();
+    }
+
+    public String getUserProviderId(){
+        return user.getProviderId();
+    }
+}
