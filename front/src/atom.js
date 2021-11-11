47796a4e35cf1b49ba379ae40facde70c49fa013
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-11-11 17:34:30 +0900

    feat: [FRONT] Tab 수정 및 prettier 설정 (#122)

--- front/src/atom.js
+++ front/src/atom.js
@@ -2,32 +2,32 @@ import { atom } from 'recoil';
 
 export const tokenState = atom({
   key: 'token',
-  default: null,
+  default: null
 });
 
 export const userState = atom({
   key: 'userInfo',
-  default: null,
+  default: null
 });
 
 export const guState = atom({
   key: 'gu',
-  default: [],
+  default: []
 });
 
 export const dongState = atom({
   key: 'dong',
-  default: [],
+  default: []
 });
 
 export const subCategoryState = atom({
   key: 'subCategory',
-  default: [],
+  default: []
 });
 
 export const storeState = atom({
   key: 'storeList',
-  default: [],
+  default: []
 });
 
 export const queryState = atom({
@@ -39,38 +39,37 @@ export const queryState = atom({
     place_name: '',
     minimum_kakao_rating: null,
     minimum_naver_rating: null,
-    minimum_instagram_hashtag: null,
-  },
+    minimum_instagram_hashtag: null
+  }
 });
 
 export const foodListState = atom({
   key: 'foodList',
-  default: [],
+  default: []
 });
 
 export const bookmarkListState = atom({
   key: 'bookmarkList',
-  default: [],
+  default: []
 });
 
 export const focusedIdState = atom({
   key: 'focusedId',
   default: {
     normal: null,
-    small: null,
-  },
+    small: null
+  }
 });
 
 export const totalState = atom({
   key: 'total',
-  default: 0,
+  default: 0
 });
 
 export const tabIdxState = atom({
   key: 'tabIdx',
-  default: 1,
-})
-
+  default: 1
+});
 
 export const coordState = atom({
   key: 'coord',
@@ -78,4 +77,4 @@ export const coordState = atom({
     lat: 37.5666805,
     lng: 126.9784147
   }
-})
+});
