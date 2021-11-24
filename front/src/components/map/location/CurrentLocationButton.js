e0c797cbd7af7c0c06bc8feb386a4561e8ccf990
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-11-24 17:09:24 +0900

    feat: [front] 현재 위치 필터 기능 추가 (#124)

--- front/src/components/map/location/CurrentLocationButton.js
+++ front/src/components/map/location/CurrentLocationButton.js
@@ -1,58 +1,65 @@
 import React from 'react';
-import { Affix } from 'antd'
-import { useRecoilState } from 'recoil'
-import { coordState } from '../../../atom'
+import { Affix } from 'antd';
+import { useRecoilState } from 'recoil';
+import { coordState, currentCoordState } from '../../../atom';
 import styled from 'styled-components';
-import GPS from '../../../assets/GPS.png'
+import GPS from '../../../assets/GPS.png';
 
 const CurrrentLocationButton = () => {
+  const [coord, setCoord] = useRecoilState(coordState);
+  const [, setCurrentCoord] = useRecoilState(currentCoordState);
 
-    const [coord, setCoord] = useRecoilState(coordState);
-
-    const onClickEvent = () => {
-        console.log("onClickEvent");
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(showPosition);
-        }
-        else {
-            alert("위치를 얻을 수 없습니다.");
-        }
+  const onClickEvent = () => {
+    console.log('onClickEvent');
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      alert('위치를 얻을 수 없습니다.');
     }
+  };
 
-    const showPosition = (position) => {
-        setCoord({
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-        })
-    }
+  const showPosition = (position) => {
+    setCoord({
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    });
+    setCurrentCoord({
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    });
+  };
 
-    const check = () => {
-        console.log(coord);
-    }
+  const check = () => {
+    console.log(coord);
+  };
 
-    return <Affix style={affixAttr}>
-
-        <GPSButton>
-            <GPSImage src={GPS} onClick={onClickEvent} />
-        </GPSButton>
+  return (
+    <Affix style={affixAttr}>
+      <GPSButton>
+        <GPSImage src={GPS} onClick={onClickEvent} />
+      </GPSButton>
     </Affix>
-}
+  );
+};
 const affixAttr = {
-    position: 'absolute',
-    bottom: 10,
-    right: 10,
-    zIndex: 2
-}
+  position: 'absolute',
+  bottom: 10,
+  right: 10,
+  zIndex: 2
+};
 
 const GPSButton = styled.button`
- width:30px;
- height:30px;
- padding:0px;
-`
+  width: 33px;
+  height: 33px;
+  padding: 3px;
+  background-color: white;
+  border-radius: 8px;
+  cursor: pointer;
+`;
 
 const GPSImage = styled.img`
-width:100%;
-height:100%;
-`
+  width: 100%;
+  height: 100%;
+`;
 
 export default CurrrentLocationButton;
--- front/src/components/map/location/CurrentMapFilterButton.js
+++ front/src/components/map/location/CurrentMapFilterButton.js
@@ -1,36 +1,72 @@
 import { Affix } from 'antd';
-import React from 'react';
-import { useRecoilState } from 'recoil'
+import React, { useState } from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import styled from 'styled-components';
 import { placeApi } from '../../../api';
-import { coordState, queryState, storeState } from '../../../atom'
+import { coordState, queryState, storeState } from '../../../atom';
+import FilterSelect from '../../filter/FilterSelect';
 
+const DISTANCE_FILTER = [
+  [0.2, '200m'],
+  [0.5, '500m'],
+  [1, '1KM']
+];
 
 const CurrentMapFilterButton = () => {
+  const coord = useRecoilValue(coordState);
+  const [query] = useRecoilState(queryState);
+  const [, setStoreList] = useRecoilState(storeState);
+  const [distance, setDistance] = useState(DISTANCE_FILTER[0]);
 
-    const [coord, setCoord] = useRecoilState(coordState);
-    const [query] = useRecoilState(queryState);
-    const [, setStoreList] = useRecoilState(storeState)
+  const onClickEvent = async () => {
+    console.log(distance[0]);
+    const { status, data, message } = await placeApi.getPlaceByLocation(
+      query,
+      coord.lat,
+      coord.lng,
+      +distance[0]
+    );
 
-    const onClickEvent = async () => {
-        const distance = 1;
-        const { status, data, message } = await placeApi.getPlaceByLocation(query, coord.lat, coord.lng, distance);
-
-        if (status === 200) {
-            setStoreList(data);
-        } else {
-            alert(message);
-        }
+    if (status === 200) {
+      setStoreList(data);
+    } else {
+      alert(message);
     }
+  };
 
-    return <Affix style={affixAttr} >
-        <button onClick={onClickEvent}>현재 지도에서 검색하기</button>
+  const onChangeFilter = (value, object) => {
+    console.log(value, object.key);
+    setDistance([object.key, value]);
+  };
+
+  return (
+    <Affix style={affixAttr}>
+      <FilterButton onClick={onClickEvent}>현재 지도에서 검색하기</FilterButton>
+      <FilterSelect
+        currentSelect={distance[1]}
+        onChangeMethod={onChangeFilter}
+        optionList={DISTANCE_FILTER}
+        width={80}
+      />
     </Affix>
-}
+  );
+};
 const affixAttr = {
-    position: 'absolute',
-    top: 10,
-    left: 10,
-    zIndex: 2
-}
+  position: 'absolute',
+  top: 10,
+  left: 10,
+  zIndex: 2
+};
 
 export default CurrentMapFilterButton;
+
+const FilterButton = styled.button`
+  font-weight: 700;
+  color: white;
+  background-color: #1890ff;
+  height: 35px;
+  border-radius: 4px;
+  padding: 6px;
+  margin-right: 8px;
+  cursor: pointer;
+`;
