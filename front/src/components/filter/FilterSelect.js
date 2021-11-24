dd5d7d55213ae9a00a01e8c5b655af180b5d012d
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-11-24 17:09:44 +0900

    chore: 프리티어 설정

--- front/src/components/filter/FilterSelect.js
+++ front/src/components/filter/FilterSelect.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { Select } from "antd";
-import { makeOption } from "../../utils/FilterUtil";
+import React from 'react';
+import { Select } from 'antd';
+import { makeOption } from '../../utils/FilterUtil';
 
-const FilterSelect = ({ currentSelect, onChangeMethod, optionList }) => {
+const FilterSelect = ({ currentSelect, onChangeMethod, optionList, width = 100 }) => {
   return (
     <Select
       value={currentSelect}
-      style={{ width: 100 }}
+      style={{ width: width }}
       onChange={onChangeMethod}
-      dropdownStyle={{ borderRadius: "0.25rem" }}
+      dropdownStyle={{ borderRadius: '0.25rem' }}
     >
       {makeOption(optionList)}
     </Select>
